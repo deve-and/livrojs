79c6a7a6fa11f442a5b936528ce26932898bc94d
Anderson Gomes <dev.andersong@gmail.com>
2023-09-26 07:43:28 -0300

    Correção do código

--- cap02/js/ex2_3.js
+++ cap02/js/ex2_3.js
@@ -7,12 +7,12 @@ frm.addEventListener("submit", (e) => {
     const veiculo = frm.inVeiculo.value;
     const preco = Number(frm.inPreco.value);
 
-    const entrada = veiculo * 0.50;
-    const parcelas = (veiculo * 0.50) / 12;
+    const entrada = preco * 0.50;
+    const parcela = (preco * 0.50) / 12;
 
     resp1.innerText = `Promoção: ${veiculo}`;
     resp2.innerText = `Entrada de R$: ${entrada.toFixed(2)}`;
-    resp3.innerText = `+ 12x de R$: ${parcelas.toFixed(2)}`;
+    resp3.innerText = `+ 12x de R$: ${parcela.toFixed(2)}`;
 
     e.preventDefault();
 })
