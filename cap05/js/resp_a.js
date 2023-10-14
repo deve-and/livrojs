d502148f5028e98d98861530f07dab69b7d1c952
Anderson Gomes <dev.andersong@gmail.com>
2023-10-14 12:32:10 +0000

    Cap05 - Exercício A (Incompleto)

---
+++ cap05/js/resp_a.js
@@ -0,0 +1,17 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h4")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const fruta = frm.inFruta.value
+    const num = Number(frm.inNumero.value)
+    const resposta = ""
+
+    for (let i = 0; i <= num; i++) {
+        if (i % 2 == 0) {
+            resposta 
+        }
+    }
+    resp.inne
+})
